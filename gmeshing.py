6eb333954e111e286789488181f60e5bd5a91fa6
Clement Audefroy <clement.audefroy@gmail.com>
2023-06-20 15:24:38 -0400

    new meshing method and new graphs

--- gmeshing.py
+++ gmeshing.py
@@ -78,16 +78,16 @@ def airfoil_gmeshing(polygon_vertices, mesh_size, n=1, show = False):
 
     return nodelisting, simplices
 
-def envelope_gmeshing (envelope_vertices, airfoil_vertices, mesh_size, n = 1, show = False):
+def envelope_gmeshing(envelope_vertices, airfoil_vertices, mesh_size, n = 1, spacing = 0, show = False):
     gmsh.initialize()
     gmsh.option.setNumber("General.Terminal", 1)
     gmsh.model.add("modele_test")
 
     npoints_profile = int(len(airfoil_vertices)/n)
     npoints_envelope = len(envelope_vertices) - 1
+    print(npoints_envelope)
+    print(npoints_profile)
     npoints = len(envelope_vertices)+len(airfoil_vertices)-1
-
-    small_envelope_vertices = np.array(([[1.5,0,-0.25],[1.5,0,0.25],[-0.5,0,0.25],[-0.5,0,-0.25]]))
 
     geom = gmsh.model.geo
 
@@ -96,75 +96,51 @@ def envelope_gmeshing (envelope_vertices, airfoil_vertices, mesh_size, n = 1, sh
                                 airfoil_vertices[vertex_index,2], mesh_size, vertex_index)
         last_index = vertex_index
 
-    for vertex_index in range(len(small_envelope_vertices)): #Adding all the points of the small envelope
-        gmsh.model.geo.addPoint(small_envelope_vertices[vertex_index,0],small_envelope_vertices[vertex_index,1],
-                                small_envelope_vertices[vertex_index,2], mesh_size*4, vertex_index+last_index+1)
+    for vertex_index in range(len(envelope_vertices)): #Adding all the points of the envelope
+        gmsh.model.geo.addPoint(envelope_vertices[vertex_index,0],envelope_vertices[vertex_index,1],
+                                envelope_vertices[vertex_index,2], mesh_size*8, vertex_index+last_index+1)
         new_last_index = vertex_index+last_index+1
-
-    #Adding the overall spline  of the small enveloppe
+    print(new_last_index)
+    #Adding the overall spline and creating the surface
     gmsh.model.geo.addLine(new_last_index, new_last_index-1, 0)
     gmsh.model.geo.addLine(new_last_index-1, new_last_index-2, 1)
     gmsh.model.geo.addLine(new_last_index-2, new_last_index-3, 2)
     gmsh.model.geo.addLine(new_last_index-3, new_last_index, 3)
     gmsh.model.geo.addCurveLoop([0, 1, 2, 3], tag=10+2*n)
-    wiretags = [10+2*n]
-
-    # Adding the overall spline of the profile and creating the surface
+    
+    # Adding the overall spline and creating the surface
+    small_envelope_vertices = np.array(([[1.5,0,-0.25 ],[1.5,0,0.25+ (n-1) * spacing],[-0.5,0,0.25+ (n-1) * spacing],[-0.5,0,-0.25 ]]))
+    for x in range(1,len(small_envelope_vertices)+1): #Adding all the points of the small envelope
+            gmsh.model.geo.addPoint(small_envelope_vertices[x-1,0],small_envelope_vertices[x-1,1], small_envelope_vertices[x-1,2], mesh_size*4, x + new_last_index )
+    
+    gmsh.model.geo.addLine(new_last_index +  1, new_last_index +  2,  6 )
+    gmsh.model.geo.addLine(new_last_index +  2, new_last_index +  3,  7 )
+    gmsh.model.geo.addLine(new_last_index +  3, new_last_index +  4,  8 )
+    gmsh.model.geo.addLine(new_last_index +  4, new_last_index +  1,  9 )
+    gmsh.model.geo.addCurveLoop([6,  7, 8,  9], tag= 2 * n + 25)
+    wiretags = [2 * n + 10]
     for i in range(n):
-        gmsh.model.geo.addSpline(np.linspace(i * npoints_profile, (i + 1) * npoints_profile - 1, npoints_profile), 2 * i+4)
-        gmsh.model.geo.addLine((i + 1) * npoints_profile - 1, i * npoints_profile, 2 * i + 5)
-        gmsh.model.geo.addCurveLoop([2 * i+4, 2 * i + 5], tag=10 * i + 2 * n + 20)
+
+
+        gmsh.model.geo.addSpline(np.linspace(i * npoints_profile, (i + 1) * npoints_profile - 1, npoints_profile), 6 * i+4)
+        gmsh.model.geo.addLine((i + 1) * npoints_profile - 1, i * npoints_profile, 6 * i + 5)
+        gmsh.model.geo.addCurveLoop([6 * i + 4, 6 * i + 5], tag=10 * i + 2 * n + 20)
         wiretags.append(10 * i + 2 * n + 20)
 
+        
     gmsh.model.geo.addPlaneSurface(wireTags= wiretags,tag = 0)
-
-
-    big_envelope_vertices = envelope_vertices
-    npoints_big_envelope = (len(big_envelope_vertices)) -1
-    npoints_small_envelope = len(small_envelope_vertices) - 1
-    
-    npoints = npoints_big_envelope + npoints_small_envelope
-
-    geom = gmsh.model.geo
-
-    for vertex_index in range(len(small_envelope_vertices)): #Adding all the points of the small envelope
-        gmsh.model.geo.addPoint(small_envelope_vertices[vertex_index,0],small_envelope_vertices[vertex_index,1],
-                                small_envelope_vertices[vertex_index,2], mesh_size*4,900+ vertex_index)
-        
-
-    for vertex_index in range(len(big_envelope_vertices)): #Adding all the points of the big envelope
-        gmsh.model.geo.addPoint(big_envelope_vertices[vertex_index,0],big_envelope_vertices[vertex_index,1],
-                                big_envelope_vertices[vertex_index,2], mesh_size*12, 910+vertex_index)
-        
-
-    #Adding the line of the big envelope
-    gmsh.model.geo.addLine(913, 912, 6)
-    gmsh.model.geo.addLine(912, 911, 7)
-    gmsh.model.geo.addLine(911, 910, 8)
-    gmsh.model.geo.addLine(910, 913, 9)
- 
-    gmsh.model.geo.addCurveLoop([6,7,8,9], tag=30+2*n)
-    wiretags2 = [30+2*n]
-
-    # Adding the line of the small envelope and creating the surface
-    
-    gmsh.model.geo.addLine(903, 902, 10)
-    gmsh.model.geo.addLine(902, 901, 11)
-    gmsh.model.geo.addLine(901, 900, 12)
-    gmsh.model.geo.addLine(900, 903, 13)
+    #gmsh.model.geo.addPlaneSurface([ 2 * n + 10,  2 * n + 25 ],tag = 1)
 
     
-    gmsh.model.geo.addCurveLoop([10,11,12,13], tag=  2 * n + 40)
-    wiretags2.append( 2 * n + 40)
 
-    gmsh.model.geo.addPlaneSurface(wireTags= wiretags2,tag = 1)
+    
 
     gmsh.model.geo.synchronize()
-
     gmsh.option.setNumber("Mesh.Smoothing", 10)
 
     gmsh.option.setNumber('Mesh.SurfaceFaces', 1)
     gmsh.option.setNumber('Mesh.Points', 1)
+
     gmsh.option.setNumber("Mesh.ElementOrder", 2)
     gmsh.model.mesh.generate(2)
 
--- graphing.py
+++ graphing.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-matplotlib.rcParams.update({'font.size': 18})
+matplotlib.rcParams.update({'font.size': 10})
 
 nmodes = 3
-lines = ['-','--','-.']
-widths = [1.0, 1.0, 1.0]
+lines = ['-','--','-.','-.',':']
+widths = [1.0, 1.0, 1.0,1.0,1.0]
 
 #Structure typique d'une ligne: [U, UR, omega, zeta, Re(lambda), Im(lambda)]
 
@@ -24,7 +24,7 @@ NACA_shifted = NACA_exp.copy()
 NACA_shifted[:,3] -= NACA_shifted[0,3]*np.ones(len(NACA_shifted[:,3]))
 NACA_nas = []
 for i in range(0,nmodes):
-    NACA_nas.append(r'TestFiles\Results\NACA0003_Curves_hydroelastic_aeroelastic'+str(i)+'.csv')
+    NACA_nas.append(r'TestFiles\Results\naca_finer_hydroelastic_aeroelastic'+str(i)+'.csv')
     NACA_nas[i] = pd.read_csv(NACA_nas[i], sep=',', header = None).to_numpy()
     NACA_nas[i][:,1] = NACA_nas[i][:,1]*95/3.4
 
@@ -35,7 +35,7 @@ F0_shifted = F0_exp.copy()
 F0_shifted[:,3] -= F0_shifted[1,3]*np.ones(len(F0_shifted[:,3]))
 F0_nas = []
 for i in range(0,nmodes):
-    F0_nas.append(r'TestFiles\Results\F0_Curves_hydroelastic_aeroelastic'+str(i)+'.csv')
+    F0_nas.append(r'TestFiles\Results\francis99_finer_hydroelastic_aeroelastic'+str(i)+'.csv')
     F0_nas[i] = pd.read_csv(F0_nas[i], sep=',', header = None).to_numpy()    
     F0_nas[i][:,1] = F0_nas[i][:,1]*250/12
   
@@ -47,143 +47,281 @@ F1_shifted = F1_exp.copy()
 F1_shifted[:,3] -= F1_shifted[1,3]*np.ones(len(F1_shifted[:,3]))
 F1_nas = []
 for i in range(0,nmodes):
-    F1_nas.append(r'TestFiles\Results\F1_Curves_hydroelastic_aeroelastic'+str(i)+'.csv')
+    F1_nas.append(r'TestFiles\Results\bergan_precis_hydroelastic_aeroelastic'+str(i)+'.csv')
     F1_nas[i] = pd.read_csv(F1_nas[i], sep=',', header = None).to_numpy()
     F1_nas[i][:,1] = F1_nas[i][:,1]*250/12
 
+
+
+
+blunt_exp = r'TestFiles\Results\Blunt_exp.csv'
+blunt_exp = pd.read_csv(blunt_exp, sep=';', header = None).to_numpy()
+blunt_exp[:,1] = blunt_exp[:,1]*10
+blunt_shifted = blunt_exp.copy()
+blunt_shifted[:,3] -= blunt_shifted[1,3]*np.ones(len(blunt_shifted[:,3]))
+blunt_nas = []
+for i in range(0,nmodes):
+    blunt_nas.append(r'TestFiles\Results\blunt_precis_hydroelastic_aeroelastic'+str(i)+'.csv')
+    blunt_nas[i] = pd.read_csv(blunt_nas[i], sep=',', header = None).to_numpy()
+    blunt_nas[i][:,1] = blunt_nas[i][:,1]*100/10
+
+blunt_simu = r'TestFiles\Results\Blunt_simu.csv'
+blunt_simu = pd.read_csv(blunt_simu, sep=';', header = None).to_numpy()
+blunt_simu[:,1] = blunt_simu[:,1]*100/10
+blunt_simu_shifted = blunt_simu.copy()
+blunt_simu_shifted[:,3] -= blunt_simu_shifted[1,3]*np.ones(len(blunt_simu_shifted[:,3]))
+
+dona_nas = []
+for i in range(0,nmodes):
+    dona_nas.append(r'TestFiles\Results\dona_finer_hydroelastic_aeroelastic'+str(i)+'.csv')
+    dona_nas[i] = pd.read_csv(dona_nas[i], sep=',', header = None).to_numpy()
+    dona_nas[i][:,1] = dona_nas[i][:,1]*100/10
+
+dona_simu = r'TestFiles\Results\Donaldson_simu.csv'
+dona_simu = pd.read_csv(dona_simu, sep=';', header = None).to_numpy()
+dona_simu[:,1] = dona_simu[:,1]*100/10
+dona_shifted = dona_simu.copy()
+dona_shifted[:,3] -= dona_shifted[1,3]*np.ones(len(dona_shifted[:,3]))
 #Plots
 
 #Part 1: Multiple modes
 
-plt.figure("Multiple modes")
+plt.figure("nacamodes",figsize=(6,9.5))
 
-ax = plt.subplot(2, 3, 1)
+
+ax = plt.subplot(2, 1, 1)
 plt.xlim(0,max(NACA_nas[0][:,1]))
 for i in range(0,nmodes):
-    plt.plot(NACA_nas[i][:,1]*1/(NACA_nas[0][0,2]/NACA_nas[i][:,2]),NACA_nas[i][:,2]/NACA_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-    plt.text(NACA_nas[i][0,1]*1/(NACA_nas[0][0,2]/NACA_nas[i][0,2]),NACA_nas[i][0,2]/NACA_nas[0][0,2]+0.05,"Hydroelastic mode "+str(i+1))
-plt.ylabel(r'$\Omega_i$')
+    plt.plot(NACA_nas[i][:,1],NACA_nas[i][:,2]/NACA_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+    plt.text(NACA_nas[i][0,1]*1/(NACA_nas[0][0,2]/NACA_nas[i][0,2]),NACA_nas[i][0,2]/NACA_nas[0][0,2]+0.05,"Hydroelastic mode "+str(i+1),fontsize = 18)
+plt.ylabel(r'$\Omega_i$',fontsize = 18)
 plt.ylim([0.6,4])
+plt.xticks([])
 
-ax = plt.subplot(2, 3, 2)
-for i in range(0,nmodes):
-    plt.plot(F0_nas[i][:,1]*1/(F0_nas[0][0,2]/F0_nas[i][:,2]),F0_nas[i][:,2]/F0_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-    plt.text(F0_nas[i][0,1]*1/(F0_nas[0][0,2]/F0_nas[i][0,2]),F0_nas[i][0,2]/F0_nas[0][0,2]+0.1,"Hydroelastic mode "+str(i+1))
-plt.xlim(0,max(F0_nas[0][:,1]))
-plt.ylim([0.4,4])
-
-ax = plt.subplot(2, 3, 3)
-plt.xlim(0,max(F1_nas[0][:,1]))
-for i in range(0,nmodes):
-    plt.plot(F1_nas[i][:,1]*1/(F1_nas[0][0,2]/F1_nas[i][:,2]),F1_nas[i][:,2]/F1_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-    plt.text(F1_nas[i][0,1]*1/(F1_nas[0][0,2]/F1_nas[i][0,2]),F1_nas[i][0,2]/F1_nas[0][0,2]+0.1,"Hydroelastic mode "+str(i+1))
-plt.ylim([0.4,4])
-
-
-
-
-plt.subplot(2, 3, 4)
+plt.subplot(2, 1, 2)
 plt.xlim(0,max(NACA_nas[0][:,1]))
 for i in range(0,nmodes):
-    plt.plot(NACA_nas[i][:,1]*1/(NACA_nas[0][0,2]/NACA_nas[i][:,2]),NACA_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-plt.xlabel(r'$U_R$')
-plt.ylabel(r'$\zeta_{i,added}$')
+    plt.plot(NACA_nas[i][:,1],NACA_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.ylabel(r'$\zeta_{i,added}$',fontsize = 18)
+plt.savefig('NACA_modes.pdf')
 
+plt.figure("f0modes",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
+for i in range(0,nmodes):
+    plt.plot(F0_nas[i][:,1],F0_nas[i][:,2]/F0_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+    plt.text(F0_nas[i][0,1]*1/(F0_nas[0][0,2]/F0_nas[i][0,2]),F0_nas[i][0,2]/F0_nas[0][0,2]+0.1,"Hydroelastic mode "+str(i+1),fontsize = 18)
+plt.xlim(0,max(F0_nas[0][:,1]))
+plt.ylim([0.4,4])
+plt.xticks([])
 
-
-plt.subplot(2, 3, 5)
+plt.subplot(2, 1, 2)
 plt.xlim(0,max(F0_nas[0][:,1]))
 for i in range(0,nmodes):
-    plt.plot(F0_nas[i][:,1]*1/(F0_nas[0][0,2]/F0_nas[i][:,2]),F0_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-plt.xlabel(r'$U_R$')
+    plt.plot(F0_nas[i][:,1],F0_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+
+plt.savefig('F0_modes.pdf')
 
 
-plt.subplot(2, 3, 6)
+plt.figure("f1modes",figsize=(6,9.5))
+
+ax = plt.subplot(2, 1, 1)
 plt.xlim(0,max(F1_nas[0][:,1]))
 for i in range(0,nmodes):
-    plt.plot(F1_nas[i][:,1]*1/(F1_nas[0][0,2]/F1_nas[i][:,2]),F1_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-plt.xlabel(r'$U_R$')
+    plt.plot(F1_nas[i][:,1],F1_nas[i][:,2]/F1_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+    plt.text(F1_nas[i][0,1]*1/(F1_nas[0][0,2]/F1_nas[i][0,2]),F1_nas[i][0,2]/F1_nas[0][0,2]+0.1,"Hydroelastic mode "+str(i+1),fontsize = 18)
+plt.ylim([0.4,4])
+plt.xticks([])
+
+ax = plt.subplot(2, 1, 2)
+plt.xlim(0,max(F1_nas[0][:,1]))
+for i in range(0,nmodes):
+    plt.plot(F1_nas[i][:,1],F1_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+
+plt.savefig('F1_modes.pdf')
 
 
-#With and without added mass vs experimental/numerical
+
+plt.figure("bluntmodes",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
+plt.xlim(0,max(blunt_nas[0][:,1]))
+for i in range(0,nmodes):
+    plt.plot(blunt_nas[i][:,1],blunt_nas[i][:,2]/blunt_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+    plt.text(blunt_nas[i][0,1]*1/(blunt_nas[0][0,2]/blunt_nas[i][0,2]),blunt_nas[i][0,2]/blunt_nas[0][0,2]+0.25,"Hydroelastic mode "+str(i+1),fontsize = 18)
+
+plt.ylim([0.6,8])
+plt.xticks([])
+
+plt.subplot(2, 1, 2)
+plt.xlim(0,max(blunt_nas[0][:,1]))
+for i in range(0,nmodes):
+    plt.plot(blunt_nas[i][:,1],blunt_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.savefig('blunt_modes.pdf')
+
+plt.figure("donamodes",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
+for i in range(0,nmodes):
+    plt.plot(dona_nas[i][:,1],dona_nas[i][:,2]/dona_nas[0][0,2], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+    plt.text(dona_nas[i][0,1]*1/(dona_nas[0][0,2]/dona_nas[i][0,2]),dona_nas[i][0,2]/dona_nas[0][0,2]+0.25,"Hydroelastic mode "+str(i+1),fontsize = 18)
+plt.ylabel(r'$\Omega_i$',fontsize = 18)
+plt.xlim(0,max(dona_nas[0][:,1]))
+plt.ylim([0.4,8])
+plt.xticks([])
+
+
+
+
+
+plt.subplot(2,1 , 2)
+plt.xlim(0,max(dona_nas[0][:,1]))
+plt.ylim([-0.005,0.06])
+for i in range(0,nmodes):
+    plt.plot(dona_nas[i][:,1],dona_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.ylabel(r'$\zeta_{i,added}$',fontsize = 18)
+plt.savefig('dona_modes.pdf')
+
 
 nmodes = 1
 
-plt.figure("Comparison")
 
-ax = plt.subplot(2, 3, 1)
+plt.figure("nacacompa",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
+
 plt.xlim(0,max(NACA_nas[0][:,1]))
 plt.scatter(NACA_exp[:,1], NACA_exp[:,2]/NACA_exp[0,2], marker="x", c='black', label="Numerical")
-plt.arrow(1, 0.97,3,-0.1)
-plt.text(5, 0.8,"Numerical")
+plt.arrow(1, 0.97,4.5,-0.25)
+plt.text(6, 0.70,"Numerical",fontsize = 18)
 plt.scatter([None],[None], marker = "v", c="black", label = "Numerical - shifted")
-plt.plot(NACA_nas[0][:,1]*1/(NACA_nas[0][0,2]/NACA_nas[0][:,2]),NACA_nas[0][:,2]/NACA_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
-plt.text(NACA_nas[0][0,1], 1.05,"Hydroelastic mode 1")
-plt.ylabel(r'$\Omega_i$')
-plt.ylim([0.6,1.5])
+plt.plot(NACA_nas[0][:,1],NACA_nas[0][:,2]/NACA_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
+plt.text(NACA_nas[0][8,1], 0.85,"Hydroelastic mode 1",fontsize = 18)
+plt.ylabel(r'$\Omega_i$',fontsize = 18)
+plt.ylim([0.6,1.2])
+plt.xticks([])
 
-ax = plt.subplot(2, 3, 2)
+plt.subplot(2, 1, 2)
+plt.xlim(0,max(NACA_nas[0][:,1]))
+
+plt.scatter(NACA_shifted[:,1], NACA_shifted[:,3], marker="v", c='black', label="Numerical - shifted")
+plt.arrow(NACA_shifted[1,1]+1, NACA_shifted[1,3]-0.01,3,-0.03)
+plt.text(NACA_shifted[1,1]+4, NACA_shifted[1,3]-0.05,"Numerical - shifted",fontsize = 18)
+for i in range(0,nmodes):
+    plt.plot(NACA_nas[i][:,1],NACA_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.ylabel(r'$\zeta_{i,added}$',fontsize = 18)
+plt.savefig('NACA_comparison.pdf')
+
+plt.figure("f0compa",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
 plt.scatter(F0_exp[:,1]*1/(F0_exp[0,2]/F0_exp[:,2]), F0_exp[:,2]/F0_exp[0,2], marker="o", c='black', label="Experimental")
 plt.arrow(0.1, 1.05,0.2, 0.1)
-plt.text(0.3, 1.2,"Experimental")
+plt.text(0.3, 1.2,"Experimental",fontsize = 18)
 plt.scatter([None],[None], marker = "^", c="black", label = "Experimental - shifted")
 fviv = 11/(0.012*F0_exp[0,2])
 plt.plot([0,fviv,2*fviv,3*fviv],[0,1,2,3], c='black', linestyle = ':', linewidth = 1.0)
-plt.text(0.85, 0.5, "Vortex shedding frequency")
-plt.plot(F0_nas[0][:,1]*F0_nas[0][:,2]/F0_nas[0][0,2],F0_nas[0][:,2]/F0_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
-plt.text(1.5,0.83,'Hydroelastic mode 1')
+plt.text(0.85, 0.5, "Vortex shedding frequency",fontsize = 18)
+plt.plot(F0_nas[0][:,1],F0_nas[0][:,2]/F0_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
+plt.text(1.5,0.83,'Hydroelastic mode 1',fontsize = 18)
 plt.xlim(0,max(F0_nas[0][:,1]))
 plt.ylim([0.4,1.5])
+plt.xticks([])
 
-ax = plt.subplot(2, 3, 3)
+plt.subplot(2, 1, 2)
+plt.xlim(0,max(F0_nas[0][:,1]))
+plt.scatter(F0_shifted[:,1]*1/(F0_exp[0,2]/F0_exp[:,2]), F0_shifted[:,3], marker="^", c='black', label="Experimental - shifted")
+plt.arrow(F0_shifted[1,1]+0.1, F0_shifted[1,3]-0.001,0.3,-0.003, width=0.0000005)
+plt.text(F0_shifted[1,1]+0.4, F0_shifted[1,3]-0.005,"Experimental - shifted",fontsize = 18)
+for i in range(0,nmodes):
+    plt.plot(F0_nas[i][:,1],F0_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.savefig('F0_comparison.pdf')
+
+plt.figure("f1compa",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
 plt.xlim(0,max(F1_nas[0][:,1]))
 plt.scatter(F1_exp[:,1]*1/(F1_exp[0,2]/F1_exp[:,2]), F1_exp[:,2]/F1_exp[0,2], marker="s", c='black', label="Experimental")
-plt.arrow(0.1, 1.03,0.175,0.1)
-plt.text(F1_exp[0,1]+0.15, 1.21,"Experimental")
+plt.arrow(2.5, 0.97,0.19,0.25)
+plt.text(F1_exp[6,1]+0.15, 1.21,"Experimental",fontsize = 18)
 plt.scatter([None],[None], marker = "P", c="black", label = "Experimental - shifted")
 fviv = 8/(0.012*F1_exp[0,2])
 plt.plot([0,fviv,2*fviv,3*fviv,4*fviv],[0,1,2,3,4], c='black', linestyle = ':', linewidth = 1.0)
-plt.text(0.8, 0.5, "Vortex shedding frequency")
-plt.plot(F1_nas[0][:,1]*F1_nas[0][:,2]/F1_nas[0][0,2],F1_nas[0][:,2]/F1_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
-plt.text(2, 0.83, "Hydroelastic mode 1")
+plt.text(0.8, 0.5, "Vortex shedding frequency",fontsize = 18)
+plt.plot(F1_nas[0][:,1],F1_nas[0][:,2]/F1_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
+plt.text(2, 0.83, "Hydroelastic mode 1",fontsize = 18)
 plt.ylim([0.4,1.5])
+plt.xticks([])
 
 
-
-
-plt.subplot(2, 3, 4)
-plt.xlim(0,max(NACA_nas[0][:,1]))
-plt.scatter(NACA_exp[:,1], NACA_exp[:,3], marker="x", c='black', label="Numerical")
-plt.scatter(NACA_shifted[:,1], NACA_shifted[:,3], marker="v", c='black', label="Numerical - shifted")
-plt.arrow(NACA_shifted[0,1]+1, NACA_shifted[0,3]-0.01,3,-0.03)
-plt.text(NACA_shifted[0,1]+5, NACA_shifted[0,3]-0.05,"Numerical - shifted")
-for i in range(0,nmodes):
-    plt.plot(NACA_nas[i][:,1]*1/(NACA_nas[0][0,2]/NACA_nas[i][:,2]),NACA_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-plt.xlabel(r'$U_R$')
-plt.ylabel(r'$\zeta_{i,added}$')
-
-
-
-plt.subplot(2, 3, 5)
-plt.xlim(0,max(F0_nas[0][:,1]))
-plt.scatter(F0_exp[:,1]*1/(F0_exp[0,2]/F0_exp[:,2]), F0_exp[:,3], marker="o", c='black', label="Experimental")
-plt.scatter(F0_shifted[:,1]*1/(F0_exp[0,2]/F0_exp[:,2]), F0_shifted[:,3], marker="^", c='black', label="Experimental - shifted")
-plt.arrow(F0_shifted[1,1]+0.1, F0_shifted[1,3]-0.001,0.3,-0.003, width=0.0000005)
-plt.text(F0_shifted[1,1]+0.4, F0_shifted[1,3]-0.005,"Experimental - shifted")
-for i in range(0,nmodes):
-    plt.plot(F0_nas[i][:,1]*1/(F0_nas[0][0,2]/F0_nas[i][:,2]),F0_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-plt.xlabel(r'$U_R$')
-
-
-plt.subplot(2, 3, 6)
+plt.subplot(2, 1, 2)
 plt.xlim(0,max(F1_nas[0][:,1]))
-plt.scatter(F1_exp[:,1]*1/(F1_exp[0,2]/F1_exp[:,2]), F1_exp[:,3], marker="s", c='black', label="Experimental")
 plt.scatter(F1_shifted[:,1]*1/(F1_exp[0,2]/F1_exp[:,2]), F1_shifted[:,3], marker="P", c='black', label="Experimental - shifted")
 plt.arrow(F1_shifted[1,1]+0.1, F1_shifted[1,3]-0.001,0.3,-0.003, width=0.0000005)
-plt.text(F1_shifted[1,1]+0.4, F1_shifted[1,3]-0.005,"Experimental - shifted")
+plt.text(F1_shifted[1,1]+0.4, F1_shifted[1,3]-0.005,"Experimental - shifted",fontsize = 18)
 for i in range(0,nmodes):
-    plt.plot(F1_nas[i][:,1]*1/(F1_nas[0][0,2]/F1_nas[i][:,2]),F1_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
-plt.xlabel(r'$U_R$')
+    plt.plot(F1_nas[i][:,1],F1_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.savefig('F1_comparison.pdf')
+
+
+plt.figure("donacompa",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
+plt.xlim(0,max(dona_nas[0][:,1]))
+plt.scatter(dona_simu[:,1], dona_simu[:,2]/dona_simu[0,2], marker="o", c='black', label="Numerical")
+plt.arrow(5.5, 1.05,2.7,0.18)
+plt.text(dona_simu[4,1]+0.15, 1.21,"Numerical",fontsize = 18)
+plt.scatter([None],[None], marker = "P", c="black", label = "Experimental - shifted")
+plt.plot(dona_nas[0][:,1],dona_nas[0][:,2]/dona_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
+plt.text(2, 0.8, "Hydroelastic mode 1",fontsize = 18)
+plt.ylim([0.4,1.5])
+plt.ylabel(r'$\Omega_i$',fontsize = 18)
+plt.xticks([])
+
+ax = plt.subplot(2, 1, 2)
+plt.xlim(0,max(dona_nas[0][:,1]))
+plt.scatter(dona_shifted[:,1], dona_shifted[:,3], marker="^", c='black', label="Numerical - shifted")
+plt.arrow(dona_shifted[2,1]+0.1, dona_shifted[2,3]-0.001,2.5,-0.01, width=0.0000005)
+plt.text(dona_shifted[2,1]+2.8, dona_shifted[2,3]-0.015,"Numerical - shifted",fontsize = 18)
+for i in range(0,nmodes):
+    plt.plot(dona_nas[i][:,1],dona_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+plt.ylabel(r'$\zeta_{i,added}$',fontsize = 18)
+plt.savefig('dona_comparison.pdf')
+
+plt.figure("bluntcompa",figsize=(6,9.5))
+ax = plt.subplot(2, 1, 1)
+plt.xlim(0,max(blunt_nas[0][:,1]))
+
+plt.scatter(blunt_exp[:,1], blunt_exp[:,2]/blunt_exp[0,2], marker="s", c='black', label="Experimental")
+plt.scatter(blunt_simu[:,1], blunt_simu[:,2]/blunt_simu[0,2], marker="x", c='black', label="Numerical")
+plt.arrow(0.1, 1.03,0.8,0.15)
+plt.text(blunt_exp[0,1]+1, 1.21,"Experimental",fontsize = 18)
+plt.arrow(5.5, 1,2.7,0.25)
+plt.text(blunt_simu[4,1]+0.15, 1.21,"Numerical",fontsize = 18)
+plt.scatter([None],[None], marker = "P", c="black", label = "Experimental - shifted")
+plt.plot(blunt_nas[0][:,1],blunt_nas[0][:,2]/blunt_nas[0][0,2], linestyle = lines[0], linewidth = widths[0], c='black', label = "Hydroelastic mode 1")
+plt.text(2, 0.8, "Hydroelastic mode 1",fontsize = 18)
+plt.ylim([0.4,1.5])
+plt.xticks([])
+
+
+
+plt.subplot(2, 1,2)
+plt.xlim(0,max(blunt_nas[0][:,1]))
+plt.ylim(-0.015,0.07)
+plt.scatter(blunt_shifted[:,1], blunt_shifted[:,3], marker="P", c='black', label="Experimental - shifted")
+plt.scatter(blunt_simu_shifted[:,1], blunt_simu_shifted[:,3], marker="v", c='black', label="Experimental - shifted")
+plt.arrow(blunt_shifted[2,1]+0.1, blunt_shifted[2,3]-0.001,1.5,-0.01, width=0.0000005)
+plt.text(blunt_shifted[2,1]+1.7, blunt_shifted[2,3]-0.015,"Experimental - shifted",fontsize = 18)
+plt.arrow(blunt_simu_shifted[3,1]+0.1, blunt_simu_shifted[3,3]-0.001,0.7,-0.02, width=0.0000005)
+plt.text(blunt_simu_shifted[3,1]+0.9, blunt_simu_shifted[3,3]-0.025,"Numerical - shifted",fontsize = 18)
+for i in range(0,nmodes):
+    plt.plot(blunt_nas[i][:,1],blunt_nas[i][:,3], linestyle = lines[i], linewidth = widths[i], c='black', label = "Hydroelastic mode "+str(i+1))
+plt.xlabel(r'$U_R$',fontsize = 18)
+
+plt.savefig('blunt_comparaison.pdf')
 
 
 plt.show()
